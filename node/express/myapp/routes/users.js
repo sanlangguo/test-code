f0dd7ecf9da575d69750953a3915aca40e3f9368
bill <Bill.Chen@shapejoy.com>
2019-12-30 21:58:19 +0800

    test router

--- node/express/myapp/routes/users.js
+++ node/express/myapp/routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req, res, next)
+  res.send('respond with a resousssssssssssssssssssrce');
+});
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  console.log(req.body.name,'req------------------')
+  if (req.body.name) {
+    res.send(res.body.name);
+  } else {
+    throw new Error('name is error')
+  }
 });
 
 module.exports = router;
